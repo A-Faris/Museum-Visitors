64ba8c5f0f59df080eba83ff54361860f19cc3d9
Faris Abulula <abululb02@gmail.com>
2024-05-08 20:03:48 +0100

    done

---
+++ coding_problem/day_3/solution.py
@@ -0,0 +1,15 @@
+"""Solution to day 3"""
+
+
+def read_input() -> list[str]:
+    """Read the list of numbers on the input file"""
+    with open("input.txt", "r", encoding="utf-8") as f:
+        return f.readlines()
+
+# Write solution below
+
+
+if __name__ == '__main__':
+    inputs = read_input()
+    print(location(inputs))
+    print(location_two(inputs))
--- pipeline folder/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "instance_public_ip" {
-  description = "Public IP address of the RDS instance"
-  value       = aws_db_instance.museum-db.address
-}
